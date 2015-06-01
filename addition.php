fd4ae44ca561c50abd8b463091dec08086ec5d4f
Rahul Singh <raul1985singh@gmail.com>
2015-06-01 15:49:07 +0530

    commiting div changes

--- addition.php
+++ addition.php
@@ -5,7 +5,15 @@ function addition($num1,$num2){
 }
 
 function division($num1,$num2){
-   return $num1 % $num2;
+   if($num1 ==0)
+   {
+	   $msg = "Div zero error";
+	   return $msg;
+   }
+   else{
+	   return $num1 % $num2;   
+   }
+  
 }
 
   $sum = addition(1,5);
