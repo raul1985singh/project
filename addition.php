df5369260a41b65b5042727aadcebe7ede45757d
Rahul Singh <raul1985singh@gmail.com>
2015-06-01 15:28:51 +0530

    commiting con1 changes

--- addition.php
+++ addition.php
@@ -4,7 +4,15 @@ function addition($num1,$num2){
    return $num1 + $num2;
 }
 
+function subtraction($num1,$num2){
+   return $num1 - $num2;
+}
+
   $sum = addition(1,5);
-  
   echo "Sum is ". $sum;
+  
+  $sub = subtraction(12,5);
+  echo "Subtraction is ". $sub;
+  
+  
 ?>
