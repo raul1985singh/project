c7a6b82b254a5f9187fabed1ddee8127d2b7253a
Rahul Singh <raul1985singh@gmail.com>
2015-06-01 15:30:30 +0530

    commiting con2

--- addition.php
+++ addition.php
@@ -4,7 +4,14 @@ function addition($num1,$num2){
    return $num1 + $num2;
 }
 
+function division($num1,$num2){
+   return $num1 % $num2;
+}
+
   $sum = addition(1,5);
-  
   echo "Sum is ". $sum;
+  
+  
+  $div = division(1,5);
+  echo "Division is ". $div;
 ?>
